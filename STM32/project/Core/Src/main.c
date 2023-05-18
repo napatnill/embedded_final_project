1c3f203c405e402fddca13b41c492083f5541735
supakorn22 <6432173821@student.chula.ac.th>
2023-05-19 00:40:41 +0700

    del dht11 function

--- STM32/project/Core/Src/main.c
+++ STM32/project/Core/Src/main.c
@@ -147,49 +147,6 @@ uint8_t DHT22_Read (GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
 }
 
 
-
-void DHT11_Start (GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
-{
-	Set_Pin_Output (GPIOx, GPIO_Pin);  // set the pin as output
-	HAL_GPIO_WritePin (GPIOx, GPIO_Pin, 0);   // pull the pin low
-	delay (18000);   // wait for 18ms
-    HAL_GPIO_WritePin (GPIOx, GPIO_Pin, 1);   // pull the pin high
-	delay (20);   // wait for 20us
-	Set_Pin_Input(GPIOx, GPIO_Pin);    // set as input
-}
-
-uint8_t DHT11_Check_Response (GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
-{
-	uint8_t Response = 0;
-	delay (40);
-	if (!(HAL_GPIO_ReadPin (GPIOx, GPIO_Pin)))
-	{
-		delay (80);
-		if ((HAL_GPIO_ReadPin (GPIOx, GPIO_Pin))) Response = 1;
-		else Response = -1; // 255
-	}
-	while ((HAL_GPIO_ReadPin (GPIOx, GPIO_Pin)));   // wait for the pin to go low
-
-	return Response;
-}
-
-uint8_t DHT11_Read (GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
-{
-	uint8_t i,j;
-	for (j=0;j<8;j++)
-	{
-		while (!(HAL_GPIO_ReadPin (GPIOx, GPIO_Pin)));   // wait for the pin to go high
-		delay (40);   // wait for 40 us
-		if (!(HAL_GPIO_ReadPin (GPIOx, GPIO_Pin)))   // if the pin is low
-		{
-			i&= ~(1<<(7-j));   // write 0
-		}
-		else i|= (1<<(7-j));  // if the pin is high, write 1
-		while ((HAL_GPIO_ReadPin (GPIOx, GPIO_Pin)));  // wait for the pin to go low
-	}
-	return i;
-}
-
 double read_dust_sensor(ADC_HandleTypeDef* hadc,GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin){
 	int voMeasured = 0;
 	double calcVoltage = 0;
